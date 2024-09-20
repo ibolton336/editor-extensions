a6a6e9a828abc2a8c85a54676db644715297d883
Ian Bolton <ibolton@redhat.com>
2024-09-20 11:10:32 -0400

    feat: Simplify React import in App.tsx

--- vscode/react/src/App.tsx
+++ vscode/react/src/App.tsx
@@ -1,15 +1,4 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello from React in VSCode Extension!</h1>
-    </div>
-  );
-};
-
-export default App;
-import React from 'react';
+import { React } from "react";
 
 const App: React.FC = () => {
   return (
