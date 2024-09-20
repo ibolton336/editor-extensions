64c26033eab0e23c357250223c03a362b86b0a93
Ian Bolton <ibolton@redhat.com>
2024-09-20 11:38:05 -0400

    feat: Add view, vscodeApi, and publicPath props to App component

--- vscode/src/views/App.tsx
+++ vscode/src/views/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-const App: React.FC = () => {
+interface AppProps {
+  view: string;
+  vscodeApi: any;
+  publicPath: string;
+}
+
+const App: React.FC<AppProps> = ({ view, vscodeApi, publicPath }) => {
   return (
     <div>
       <h1>Konveyor GUI</h1>
       <p>This is a React component in your VSCode extension!</p>
+      <p>View: {view}</p>
+      <p>Public Path: {publicPath}</p>
     </div>
   );
 };
